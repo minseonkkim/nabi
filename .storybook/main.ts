b7eb10d21afd2192ae6d804a7c69a789cf6e557d
minseonkkim <minsun9856@gmail.com>
2025-01-21 13:14:51 +0900

    remove: 필요없는 파일 삭제

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,7 +1,8 @@
 import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ['../src/components/**/*.stories.@(js|jsx|ts|tsx)',
+    '../src/components/**/*.mdx',],
   addons: [
     "@storybook/addon-onboarding",
     "@storybook/addon-essentials",
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -2,10 +2,10 @@ import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
   parameters: {
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
+    viewMode: 'docs', 
+    options: {
+      storySort: {
+        order: ['Documentation', 'Components'], 
       },
     },
   },
